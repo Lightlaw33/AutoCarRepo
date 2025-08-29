ae658e90239693ba0f7eb5d043da924c739d0132
Lightlaw33 <luizfeberaldo@gmail.com>
2025-08-29 10:10:42 -0300

    Adicionado os atributos nos construtores

--- src/avaliacao_leandro/ClasseAutomotores.java
+++ src/avaliacao_leandro/ClasseAutomotores.java
@@ -1,5 +1,23 @@
 package avaliacao_leandro;
 
 public class ClasseAutomotores {
+	String marcaAt;
+	String modeloAt;
+	String tipoAt;
+	String corAt;
 
+
+	public ClasseAutomotores() {
+	
+	}
+	
+	public ClasseAutomotores(String marcaPar, String modeloPar, String tipoPar, String corPar) {
+		
+		this.marcaAt = marcaPar;
+		this.modeloAt = modeloPar;
+		this.tipoAt = tipoPar;
+		this.corAt = corPar;
+		
+		
+	}
 }
