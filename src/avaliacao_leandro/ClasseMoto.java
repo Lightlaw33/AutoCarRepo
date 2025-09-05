6eb703b1759fe4cb416ebc3afce08a836fb24b3f
Lightlaw33 <luizfeberaldo@gmail.com>
2025-09-05 10:31:41 -0300

    Adicionado subclasse ClasseMoto

---
+++ src/avaliacao_leandro/ClasseMoto.java
@@ -0,0 +1,13 @@
+package avaliacao_leandro;
+
+public class ClasseMoto extends ClasseAutomotores{
+	
+	
+public ClasseMoto(String corPar, String tipoPar, String modeloPar, String marcaPar, float cargaSuportadaPar) {
+		
+		super(corPar, tipoPar, marcaPar, modeloPar);
+		
+		
+	}
+
+}
